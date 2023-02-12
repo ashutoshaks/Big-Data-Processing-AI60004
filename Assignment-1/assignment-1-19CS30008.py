f2e5d9c0e386f8c12c36864857a98401b1073e86
ashutoshaks <ashu11.03.2002@gmail.com>
2023-02-12 12:19:08 +0530

    Update assignment 1

--- Assignment-1/assignment-1-19CS30008.py
+++ Assignment-1/assignment-1-19CS30008.py
@@ -1,20 +1,27 @@
+# Ashutosh Kumar Singh
+# 19CS30008
+
 import re
 import sys
 import os
 import threading
 import heapq
-import time
-
-t1 = time.time()
+import math
 
 if len(sys.argv) != 5:
-    print("Usage: python assignment-1-19CS30008.py <path to data directory> <# threads> <value of n for n-grams> <value of k>")
+    print('Usage: python assignment-1-19CS30008.py <path to data directory> <# threads> <value of n for n-grams> <value of k>')
     exit(1)
-data_path = sys.argv[1]
-num_threads = int(sys.argv[2])
-n = int(sys.argv[3])
-k = int(sys.argv[4])
-# print(data_path, num_threads, n, k)
+data_path = sys.argv[1]  # path to data directory
+num_threads = int(sys.argv[2])  # number of threads
+n = int(sys.argv[3])    # value of n for n-grams
+k = int(sys.argv[4])    # value of k
+
+if n < 1:
+    print('Value of n should be greater than or equal to 1')
+    exit(1)
+if k < 1:
+    print('Value of k should be greater than or equal to 1')
+    exit(1)
 
 classes = []
 num_docs = {}
@@ -27,55 +34,32 @@ for dir in os.listdir(data_path):
         for file in curr_files:
             files.append(os.path.join(data_path, dir, file))
 
-# print(classes)
-# print(num_docs)
-# print(len(classes))
-# print(len(files))
-
-ngram_dict = {}
+ngram_dict = {}  # dictionary having an entry for each document class, each entry in itself is a dictionary storing n-grams and their corresponding counts
 lock = {}
 for cls in classes:
     ngram_dict[cls] = {}
     lock[cls] = threading.Lock()
 
-
+# Returns n-grams for a given list of words
 def get_ngrams(words, n):
     if len(words) < n:
         return []
 
     ngrams = []
-    # for i in range(len(words) - n + 1):
-    #     ngrams.append(tuple(words[i: i + n]))
-    # return ngrams
-
-    curr_ngram = []
+    curr_ngram = ''
     for i in range(n):
-        curr_ngram.append(words[i])
-    ngrams.append(tuple(curr_ngram))
+        curr_ngram += words[i] + ' '
+    ngrams.append(curr_ngram[:-1])
     for i in range(n, len(words)):
-        curr_ngram.pop(0)
-        curr_ngram.append(words[i])
-        ngrams.append(tuple(curr_ngram))
-
-    # if len(words) < n:
-    #     return []
-    # ngrams = []
-    # curr_ngram = []
-    # for i in range(n):
-    #     curr_ngram.append(words[i])
-    # ngrams.append(' '.join(curr_ngram))
-    # # for i in range(len(words) - n + 1):
-    # for i in range(n, len(words)):
-    #     # ngrams.append(tuple(words[i: i + n]))
-    #     curr_ngram.pop(0)
-    #     curr_ngram.append(words[i])
-    #     ngrams.append(' '.join(curr_ngram))
-
+        prev_len = len(words[i - n])
+        curr_ngram = curr_ngram[prev_len + 1:]
+        curr_ngram += words[i] + ' '
+        ngrams.append(curr_ngram[:-1])
     return ngrams
 
-
+# Task to be performed by each thread
+# For the files assigned to it, it reads the file, extracts words, gets n-grams and updates the n-gram dictionary
 def task(id, start, end):
-    print(f"Thread {id} started")
     for i in range(start, end + 1):
         file = files[i]
         with open(file, 'r', encoding='utf-8', errors='replace') as f:
@@ -83,30 +67,25 @@ def task(id, start, end):
             words = re.findall(r'[a-zA-Z0-9]+', file_str)
             words = [word.lower() for word in words]
             ngrams = get_ngrams(words, n)
-            # print(ngrams)
-            cls = os.path.basename(os.path.dirname(
-                os.path.join(data_path, file)))
-            # lock[cls].acquire()
+            cls = os.path.basename(os.path.dirname(file))
             for ngram in ngrams:
-                # if ngram[0] == 'ax' and ngram[1] == 'ax' and ngram[2] == 'ax' and ngram[3] == 'ax' and ngram[4] == 'ax':
-                #     print(cls)
-                #     print(file)
                 lock[cls].acquire()
                 if ngram in ngram_dict[cls]:
                     ngram_dict[cls][ngram] += 1
                 else:
                     ngram_dict[cls][ngram] = 1
                 lock[cls].release()
-            # lock[cls].release()
-    print(f"Thread {id} finished")
-
 
 threads = []
-chunk_size = len(files) // num_threads
+chunk_size = int(math.ceil(len(files) / num_threads))
+if chunk_size == 0:
+    chunk_size = 1
+
+print('Threads started')
+
 for i in range(num_threads):
-    start = i * chunk_size
-    end = (i + 1) * chunk_size - 1
-    # print(start, end)
+    start = min(i * chunk_size, len(files))
+    end = min((i + 1) * chunk_size - 1, len(files) - 1)
     t = threading.Thread(target=task, args=(i + 1, start, end))
     threads.append(t)
     t.start()
@@ -114,48 +93,25 @@ for i in range(num_threads):
 for t in threads:
     t.join()
 
-t2 = time.time()
+print('Threads finished')
 
-# for key, value in ngram_dict.items():
-#     print(key, len(value))
-    # print(value)
-
-# score = count of ngram in a class / no. of documents in the class
-
-# (26.988)
-ngram_scores = {}
-ngram_class = {}
+ngram_scores = {}   # dictionary storing n-grams and their corresponding scores
+ngram_class = {}    # dictionary storing n-grams and their corresponding class with the highest score
 for key, value in ngram_dict.items():
     for ngram, count in value.items():
         if ngram in ngram_scores:
             if ngram_scores[ngram] < count / num_docs[key]:
                 ngram_class[ngram] = key
                 ngram_scores[ngram] = count / num_docs[key]
-            # ngram_scores[ngram] = max(ngram_scores[ngram], count / num_docs[key])
         else:
             ngram_class[ngram] = key
             ngram_scores[ngram] = count / num_docs[key]
 
-# # get top k ngrams
-# top_k_ngrams = sorted(ngram_scores.items(),
-#                       key=lambda x: x[1], reverse=True)[:k]
-
-t3 = time.time()
-
-# using heapq (16.987)
+# Get the top k n-grams using a heap
 ngs = [(-score, ngram) for ngram, score in ngram_scores.items()]
 top_k_ngrams = heapq.nsmallest(k, ngs)
 
-top_k_cls = []
-for score, ngram in top_k_ngrams:
-    top_k_cls.append(ngram_class[ngram])
-# for ngram, score in top_k_ngrams:
-#     top_k_cls.append(ngram_class[ngram])
-print(top_k_ngrams)
-print(top_k_cls)
-
-t4 = time.time()
-
-print(f"Time taken for preprocessing: {t2 - t1}")
-print(f"Time taken for scoring: {t3 - t2}")
-print(f"Time taken for getting top k: {t4 - t3}")
+print(f'\nTop {k} n-grams (n = {n}):')
+print('(n-gram, score, class)')
+for i, (score, ngram) in enumerate(top_k_ngrams):
+    print(f'{i + 1}. ({ngram}, {round(-score, 4)}, {ngram_class[ngram]})')
