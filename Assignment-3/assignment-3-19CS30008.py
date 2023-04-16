6fd007a0482a53292550e0a3dc3b1e95acec929e
ashutoshaks <ashu11.03.2002@gmail.com>
2023-04-17 01:50:58 +0530

    Update assignment 3

--- Assignment-3/assignment-3-19CS30008.py
+++ Assignment-3/assignment-3-19CS30008.py
@@ -1,46 +1,35 @@
-'''
-pyspark code using RDDs to compute top k words with highest pointwise mutual information (PMI) with a given word in a set of documents. 
-Remove stopwords which are given in a different file.
-In another file, each line represents one document.
-'''
-
 from pyspark import SparkConf, SparkContext
 import sys
 import math
+import re
 
 
 def filter_stopwords_unique(doc, stopwords):
-    '''
-    Filter stopwords from a document and return a list of unique words
-    '''
     words = doc.split()
     uniq_words = set([word.lower() for word in words if word not in stopwords])
     return ' '.join(uniq_words)
 
 
 def get_pairs(doc, query_word):
-    '''
-    Return a list of (word, query_word) pairs for each word in the document
-    '''
     words = doc.split()
     cnt = 1 if query_word in words else 0
     return [((query_word, word), cnt) for word in words if word != query_word]
 
 
 def pmi(co_count, w1_count, w2_count, docs_count):
-    '''
-    Compute the pointwise mutual information (PMI) for two words
-    '''
     if co_count == 0:
-        return -1e6
+        return -math.inf
     return math.log2((co_count * docs_count) / (w1_count * w2_count))
 
 
-if __name__ == "__main__":
-    # Create Spark context with Spark configuration
+def main():
     conf = SparkConf().setMaster("local").setAppName(
         "PMI top k and lowest k for a given word")
     sc = SparkContext(conf=conf)
+
+    if len(sys.argv) != 5:
+        print("Usage: assignment-3-19CS30008.py <docs_path> <query_word> <k> <stopwords_path>")
+        sys.exit(1)
 
     docs_path = sys.argv[1]
     query_word = sys.argv[2]
@@ -50,11 +39,13 @@ if __name__ == "__main__":
     # Read the documents file
     docs_rdd = sc.textFile(docs_path)
     stopwords_rdd = sc.textFile(stopwords_path)
-    stopwords = set(stopwords_rdd.collect())
+    stopwords = stopwords_rdd.collect()
+    stopwords = set([word.lower() for word in stopwords])
 
     docs_rdd = docs_rdd.map(
         lambda doc: filter_stopwords_unique(doc, stopwords))
-    words_rdd = docs_rdd.flatMap(lambda doc: doc.split())
+    words_rdd = docs_rdd.flatMap(lambda doc: doc.split()).filter(
+        lambda word: re.match(r'^[a-zA-Z]+$', word))
 
     # Compute the number of documents
     docs_count = docs_rdd.count()
@@ -62,13 +53,8 @@ if __name__ == "__main__":
     word_count_rdd = words_rdd.map(lambda word: (
         word, 1)).reduceByKey(lambda x, y: x + y)
 
-    # Documents containing the query word
-    # docs_with_query_word = docs_rdd.filter(
-    #     lambda doc: query_word in doc.split())
-
     word_pairs_rdd = docs_rdd.flatMap(
         lambda doc: get_pairs(doc, query_word))
-    print(word_pairs_rdd.collect())
     word_pairs_counts_rdd = word_pairs_rdd.reduceByKey(lambda x, y: x + y)
 
     # ((w1, w2), (co_count, w1_count))
@@ -77,22 +63,35 @@ if __name__ == "__main__":
     # ((w1, w2), w2_count)
     word2_counts_rdd = word_pairs_counts_rdd.map(lambda x: (x[0][1], (x[0][0], x[1]))).join(
         word_count_rdd).map(lambda x: ((x[1][0][0], x[0]), x[1][1]))
-    
-    # print(word1_counts_rdd.collect())
-    # print(word2_counts_rdd.collect())
 
-    # (co, w1, w2)
-    co_w1_w2_rdd = word1_counts_rdd.join(word2_counts_rdd).map(lambda x: (x[0], (x[1][0][0], x[1][0][1], x[1][1])))
-
-    print(co_w1_w2_rdd.collect())
+    # ((w1, w2), (co, w1, w2))
+    co_w1_w2_rdd = word1_counts_rdd.join(word2_counts_rdd).map(
+        lambda x: (x[0], (x[1][0][0], x[1][0][1], x[1][1])))
 
     # Compute PMI for each word pair
-    pmi_rdd = co_w1_w2_rdd.map(lambda x: (x[0], pmi(x[1][0], x[1][1], x[1][2], docs_count)))
-    print(pmi_rdd.collect())
+    pmi_rdd = co_w1_w2_rdd.map(lambda x: (
+        x[0], pmi(x[1][0], x[1][1], x[1][2], docs_count)))
 
-    # get top k and lowest k words
+    # Remove the entries with -inf
+    pmi_rdd = pmi_rdd.filter(lambda x: x[1] != -math.inf)
+
+    # Get top k and lowest k words
     top_k = pmi_rdd.takeOrdered(k, key=lambda x: -x[1])
     bottom_k = pmi_rdd.takeOrdered(k, key=lambda x: x[1])
 
-    print("Top k words with highest PMI with the query word: ", top_k)
-    print("Bottom k words with lowest PMI with the query word: ", bottom_k)
+    print()
+    print("Top k words with highest (positive) PMI with the query word: ")
+    for e in top_k:
+        if e[1] > 0:
+            print(e[0][1], e[1])
+    print()
+
+    print("Bottom k words with lowest (negative) PMI with the query word: ")
+    for e in bottom_k:
+        if e[1] < 0:
+            print(e[0][1], e[1])
+    print()
+
+
+if __name__ == "__main__":
+    main()
